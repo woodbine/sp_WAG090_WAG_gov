9caaacc97b14063757fdb378c92539b21d61f0cd
woodbine <ian@spendnetwork.com>
2015-04-12 19:52:58 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -39,27 +39,20 @@ for block in blocks:
 	
 	for fileBlock in fileBlocks:
 		fileUrl = fileBlock.a['href']
-		fileUrl = fileUrl.replace("/government","http://www.gov.uk/government")
-		fileUrl = fileUrl.replace(".csv/preview",".csv")
+		title = fileBlock.a.contents[0]
 		
-		title = fileBlock.h2.contents[0]
-		titleTest = title.find('Download CSV')
+		# create the right strings for the new filename
+		title = title.upper().strip()
+		csvYr = title.split(' ')[-1]
+		csvYr = csvYr.replace("200","20")
 		
-		if titleTest == None:
-			print 'not a csv'
-		else:
-			# create the right strings for the new filename
-			title = title.upper().strip()
-			csvYr = title.split(' ')[-1]
-			csvYr = csvYr.replace("200","20")
-			
-			csvMth = title.split(' ')[-2][:3]
-			csvMth = convert_mth_strings(csvMth);
+		csvMth = title.split(' ')[-2][:3]
+		csvMth = convert_mth_strings(csvMth);
+	
+		filename = entity_id + "_" + csvYr + "_" + csvMth
+	
+		todays_date = str(datetime.now())
+	
+		scraperwiki.sqlite.save(unique_keys=['l'], data={"l": fileUrl, "f": filename, "d": todays_date })
 		
-			filename = entity_id + "_" + csvYr + "_" + csvMth
-		
-			todays_date = str(datetime.now())
-		
-			scraperwiki.sqlite.save(unique_keys=['l'], data={"l": fileUrl, "f": filename, "d": todays_date })
-			
-			print filename
+		print filename
